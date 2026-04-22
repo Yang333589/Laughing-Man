2a55de53e56bb06c14cbd95e4107c51b7c26eef5
Monica Yang <1212monicayang@gmail.com>
2026-04-22 10:39:49 -0400

    Starting Speed

--- laughingMan.js
+++ laughingMan.js
@@ -1,4 +1,6 @@
 const SQUARE_COUNT = 3;
+const TIMER_SPEED = 10;
+const SPEED = 5;
 
 document.addEventListener("DOMContentLoaded", () =>{
     document.querySelector("#square").addEventListener("click", ()=>{
@@ -15,4 +17,22 @@ document.addEventListener("DOMContentLoaded", () =>{
         square.className = "square";
         box.appendChild(square);
     }
+
+    //forEach runs a function
+    Array.from(box.children).forEach((element)=>{
+        let dx = SPEED* Math.random()*2-1; //this gives a number from -1 to 0.99999
+        let dy = SPEED* Math.random()*2-1;
+
+        let x = parseInt(element.style.left) || 225;
+        let y = parseInt(element.style.top) || 175;
+
+        //setInterval takes a function and a period of time
+        setInterval(()=>{
+            x += dx;
+            y += dy;
+
+            element.style.left = x + "px";
+            element.style.top = y + "px";
+        }, TIMER_SPEED)
+    });
 });
